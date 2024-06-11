51ad53fc751193d84d987a1a419ec142cb15ed2f
hasnainahmad0077 <hasnainahmad0077@gmail.com>
2024-06-11 18:58:51 +0500

    class01

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+var first_name = "hamza";
+var last_name = "Khan";
+let age = 25;
+console.log(first_name, last_name, "is", age, "years old");
+//implicit or explicit type
+//Arthimatic operators (+,-,*,/,%,**)
+let num1 = 5;
+let num2 = 3;
+//console.log(num1+num2);
+//console.log(num1-num2);
+//console.log(num1*num2);
+//console.log(num1/num2);
+let result = num1 % num2;
+console.log("Reminder or Modulus is", result);
+let res = num1 ** num2;
+console.log("the Power is", res);
---
+++ index.ts
@@ -0,0 +1,37 @@
+//var first_name = "hamza"
+//var last_name = "Khan"
+//let age = 25;
+
+//console.log(first_name,last_name,"is",age,"years old")
+
+//implicit or explicit type
+//Arthimatic operators (+,-,*,/,%,**)
+
+//let num1: number = 5
+//let num2: number = 3
+//console.log(num1+num2);
+//console.log(num1-num2);
+//console.log(num1*num2);
+//console.log(num1/num2);
+//let result = num1%num2;
+//console.log("Reminder or Modulus is",result)
+
+//Bedmas
+//bracket, exponent or power, Division, Multiplication, Addition, subtraction
+//left to write multiplication or Division 
+
+//let res = num1**num2
+//console.log("the Power is",res)
+
+// Assignment operators =, +=, -=, *=, /= 
+//let num1
+
+
+
+
+
+
+
+
+
+
